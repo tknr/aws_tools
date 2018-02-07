64e867c2b3e846e4323326e38541fc022b9457e3
hirai <hirai@kokochie.jp>
2018-02-07 11:52:32 +0900

    2018年 2月 7日 水曜日 11時52分32秒 JST

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ln -s `pwd`/ec2_ip_list_danvi ~/bin/
+ln -s `pwd`/ec2_ip_list_chcl ~/bin/
 ln -s `pwd`/ec2_ip_list_tfhd ~/bin/
 ln -s `pwd`/ec2_ip_list_keiretsu ~/bin/
 ln -s `pwd`/ec2_ip_list_olivers ~/bin/
