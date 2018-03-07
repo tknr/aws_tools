2a147ac5863f405cfad76a81c2a2346f05f55b5d
hirai <hirai@kokochie.jp>
2018-03-07 12:41:08 +0900

    2018年 3月 7日 水曜日 12時41分08秒 JST

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-ln -s `pwd`/aws_ec2_ip_list ~/bin/
-ln -s `pwd`/aws_ssl_list ~/bin/
+#!/bin/bash -x
+ln -s `pwd`aws_list ~/bin/
+ln -s `pwd`aws_ec2_ip_list ~/bin/
+ln -s `pwd`aws_ssl_list ~/bin/
 
 
